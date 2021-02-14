79cb7c4d55669beea29d52c1fb25a027020dd3c1
lnfin <dalmannikita@gmail.com>
2021-02-14 13:53:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-import csv
-import pandas as pd
 
 PATH_TO_FILES = r'data/'
 
@@ -8,8 +6,15 @@ fields = ['day;HH:MM:SS;gru\n']
 for file in os.listdir(PATH_TO_FILES):
     with open(PATH_TO_FILES + file, 'r', encoding="ISO-8859-1") as f:
         data = [el.split() for el in f.readlines()[2:]]
+        res = []
         for fd in data:
-            fields.append(f'{file[-6:-4]};{fd[1]};{fd[2]}\n')
+            if fd[1][0:2] in ["00","01","02"]:
+                res.append([fd[1], fd[2]])
+        res = [[el[0], float(el[1])] for el in res]
+        res.sort(key=lambda x: x[1], reverse=True)
+        print(res)
+        for el in res:
+            fields.append(f'{file[-6:-4]};{el[0]};{el[1]}\n')
 
 file = open('task_1.csv', 'w')
 file.writelines(fields)
